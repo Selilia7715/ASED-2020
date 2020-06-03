e73dcabcafd9995c09b35a4662b751608fac8110
Selilia7715 <wendy19980407@gmail.com>
2020-06-03 22:07:30 +0900

    Script/酸フォルダのASED.csのSource内容追加

--- ASED/Assets/Script/酸/ASED.cs
+++ ASED/Assets/Script/酸/ASED.cs
@@ -14,23 +14,59 @@ public class ASED : MonoBehaviour {
         LEFT,
     }
 
+    Rigidbody2D rb;
+
     // 速度
     public float speed;
-    // 動く方向
+
     public MOVE_DISTANCE dis;
 
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
     void Update()
     {
+        Move();
+    }
+
+    //当たり判定
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        //敵に当たったら
+        if (collision.gameObject.tag == "Pipe")
+        {
+            // 1.5秒後に破壊
+            Destroy(collision.gameObject,1.5f);
+        }
+    }
+
+    void Move()
+     {
+        // どの方向に移動するか
+        switch (dis)
+        {
+            case MOVE_DISTANCE.TOP:
+                rb.velocity = new Vector2(rb.velocity.x, speed);
+                break;
+
+            case MOVE_DISTANCE.DOWN:
+                rb.velocity = new Vector2(rb.velocity.x, -speed);
+                break;
+
+            case MOVE_DISTANCE.RIGHT:
+                rb.velocity = new Vector2(speed, rb.velocity.y);
+                break;
+
+            case MOVE_DISTANCE.LEFT:
+                rb.velocity = new Vector2(-speed, rb.velocity.y);
+                break;
+
+            default:
+                break;
+        }
+
     }
 }
 
 
-//   //当たり判定
-//   void OnCollisionEnter2D(Collision2D collision)
-//   {
-//       //敵に当たったら
-//       if (collision.gameObject.tag == "Pipe")
-//       {
-//           Destroy(collision.gameObject);
-//       }
-//   }
